be254e8ba16ea652274373a708aa4d8127dc2e9e
Nazariy Vavryk <nazariy@inbox.ru>
2020-03-01 13:11:44 +0000

    aztec enabled

--- src/App.js
+++ src/App.js
@@ -22,6 +22,60 @@ const Centered = styled("div", {
 });
 
 class App extends React.Component {
+  state = {
+    sdkLoaded: false,
+    account: null,
+    network: null
+  };
+
+  enableAztecSdk = async () => {
+    // const {
+    //   network,
+    // } = this.state;
+    try {
+      const contractAddresses = {
+        ganache: {
+          ACE: "0xFA8eD6F76e8f769872a1f8a89085c56909EC8Cfc", //getContractAddress('ACE', network),
+          AccountRegistryManager: "0x66db0e20a9d619ee3dfa3819513ab8bed1b21a87" //getContractAddress('AccountRegistryManager', network),
+        }
+      };
+      await window.aztec.enable({
+        apiKey: "ethglobalstarterkit",
+        contractAddresses
+      });
+    } catch (error) {
+      console.error("Failed to enable AZTEC SDK.");
+      console.log(error);
+      this.setState({
+        error
+      });
+    }
+  };
+
+  componentDidMount = () => {
+    if (window.aztec) {
+      this.aztecSdkLoaded();
+    } else {
+      window.aztecCallback = this.aztecSdkLoaded;
+    }
+  };
+
+  aztecSdkLoaded = () => {
+    // window.aztec.addListener("profileChanged", this.handleProfileChanged);
+
+    const account = window.aztec.web3.getAccount();
+    const network = window.aztec.web3.getNetwork();
+
+    this.setState(
+      {
+        sdkLoaded: true,
+        account,
+        network
+      },
+      this.enableAztecSdk
+    );
+  };
+
   render = () => (
     <StyletronProvider value={engine}>
       <BaseProvider theme={LightTheme}>
